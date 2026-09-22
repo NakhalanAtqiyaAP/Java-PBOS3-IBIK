7587faa4f35f4e39ecc8a86330650ec384003102
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2026-09-22 21:30:09 +0700

    Rename Folder: Pertemuan1 to Praktikum1

--- Praktikum/Praktikum1/latihan/latihan1.java
+++ Praktikum/Praktikum1/latihan/latihan1.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class latihan1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("=== Selamat Datang Di Toko Baju ===");
+        
         
     }
 }
