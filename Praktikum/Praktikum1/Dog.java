4ed6bfe975b2d2ad237be29ef8ea37e2fec45bb8
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2026-09-22 21:54:00 +0700

    add: New file Dog

---
+++ Praktikum/Praktikum1/Dog.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Dog {
+    public static void main(String[] args) {
+        String breed;
+        int age;
+        String color;
+
+        void barking(){
+            System.out.println("Woof..woof");
+        }
+
+        void hungry(){
+            System.out.println("Woof... humph");
+        }
+
+        void playTime(){
+            System.out.println("Woof..woof...woof");
+        }
+
+        
+    }
+}
