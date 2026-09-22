422e8a7f619c4ced971f7c687bff9450fa830c0a
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2026-09-22 21:49:33 +0700

    add: New variable name

--- Praktikum/Praktikum1/Introduce.java
+++ Praktikum/Praktikum1/Introduce.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 public class Introduce {
     public static void main(String[] args) {
         int umur = 10;
+        String nama = "Nakhalan";
 
         System.out.println("Contoh Penggunaan variable: " + umur);
 
     //Operator Pembanding
     boolean cekCukupUmur = umur > 10;
+    boolean cekNama = nama == "Nakhalan";
 
-    if (cekCukupUmur) {
+    if (cekCukupUmur && cekNama ) {
         System.out.println("Kamu boleh masuk");
     } else {
         System.out.println("Kamu tidak boleh masuk");
     }
+
+
 
     }
 }
