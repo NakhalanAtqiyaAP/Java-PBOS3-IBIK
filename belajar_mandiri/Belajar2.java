9cd6d77aca89d858e0136e08f8fe91f35c0acc6e
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2026-09-24 16:51:06 +0700

    Belajar Array

--- belajar_mandiri/Belajar2.java
+++ belajar_mandiri/Belajar2.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 public class Belajar2 {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-        
+    
+    String[] daftarMakanan = new String[3];
+
+    daftarMakanan[0] = "Sate";
+    daftarMakanan[1] = "Soto";
+    daftarMakanan[2] = "Nasi padang";
         
     }
 }
